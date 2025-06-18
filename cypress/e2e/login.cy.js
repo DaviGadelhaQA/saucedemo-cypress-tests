5e60ccbb8dcb52cf7a6359edd0b83fbe4610a13e
DavigadelhaDev <davigadelhadev@gmail.com>
2025-06-18 19:05:24 -0300

    Add test case TCF_001: Login with valid credentials

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,14 @@
-describe("Login_funcionality", () => {
-    it('Should make login as sucess', () => {
-        cy.visit('/')
-    })
-})
+describe("Login Functionality", () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it("Should log in successfully", () => {
+    cy.get('[data-test="username"]').type(Cypress.env('Username'));
+    cy.get('[data-test="password"]').type(Cypress.env('Password'));
+    cy.get('[data-test="login-button"]').click();
+
+    cy.url().should('include', '/inventory.html');
+    cy.get('[data-test="title"]').should('contain', 'Products');
+  });
+});
