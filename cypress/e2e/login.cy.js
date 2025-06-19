c79fa0237433de7d536cae90c71e55ce036d9e5c
DavigadelhaDev <davigadelhadev@gmail.com>
2025-06-19 08:52:39 -0300

    add login tests for trimmed input and invalid username (TCF_002, TCF_003)

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,12 +3,29 @@ describe("Login Functionality", () => {
     cy.visit('/');
   });
 
-  it("Should log in successfully", () => {
-    cy.get('[data-test="username"]').type(Cypress.env('Username'));
-    cy.get('[data-test="password"]').type(Cypress.env('Password'));
-    cy.get('[data-test="login-button"]').click();
+  it("TCF_001 - Login with valid credentials", () => {
+    cy.login();
 
     cy.url().should('include', '/inventory.html');
-    cy.get('[data-test="title"]').should('contain', 'Products');
+    cy.get('.title').should('contain', 'Products');
+  });
+
+  it("TCF_002 - Login with trimmed username and password", () => {
+    cy.loginWithTrimmedCredentials();
+
+    cy.get('[data-test="error"]').should('be.visible')
+      .and('contain', 'Epic sadface: Username and password do not match any user in this service');
+  });
+
+  it("TCF_003 - Login with invalid username and valid password", () => {
+    cy.fillFields({
+      '[data-test="username"]': 'invalid_user',
+      '[data-test="password"]': Cypress.env('Password'),
+    });
+
+    cy.get('[data-test="login-button"]').click();
+
+    cy.get('[data-test="error"]').should('be.visible')
+      .and('contain', 'Epic sadface: Username and password do not match any user in this service');
   });
 });
---
+++ cypress/support/commands.js
@@ -0,0 +1,23 @@
+Cypress.Commands.add('fillFields', (fields) => {
+  Object.entries(fields).forEach(([selector, value]) => {
+    cy.get(selector).type(value);
+  });
+});
+
+Cypress.Commands.add('login', () => {
+  cy.fillFields({
+    '[data-test="username"]': Cypress.env('Username'),
+    '[data-test="password"]': Cypress.env('Password'),
+  });
+
+  cy.get('[data-test="login-button"]').click();
+});
+
+Cypress.Commands.add('loginWithTrimmedCredentials', () => {
+  cy.fillFields({
+    '[data-test="username"]': `  ${Cypress.env('Username')}  `,
+    '[data-test="password"]': `  ${Cypress.env('Password')}  `,
+  });
+  
+  cy.get('[data-test="login-button"]').click();
+});
